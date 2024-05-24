a50b3e3692099ac45cf121235929914a06cb3415
Gerardo Lanza <glanza007@gmail.com>
2024-05-24 11:39:19 -0600

    crud categorias

---
+++ Biblioteca/Biblioteca.Frontend/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Frontend.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Categoria")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Fecha de Creación")]
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    }
+}
