cfb1f413f09c640bb88741ac2c352d45bb5167f0
Gerardo Lanza <glanza007@gmail.com>
2024-05-27 15:26:13 -0600

    dashboard y background

--- Biblioteca/Biblioteca.Frontend/Controllers/HomeController.cs
+++ Biblioteca/Biblioteca.Frontend/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Biblioteca.Frontend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Biblioteca.Frontend.Controllers
@@ -7,14 +8,35 @@ namespace Biblioteca.Frontend.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7055/");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var response = await _httpClient.GetAsync("/api/Prestamos");
+            var responseLibros = await _httpClient.GetAsync("/api/Libros");
+            if (response.IsSuccessStatusCode && responseLibros.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var prestamos = JsonConvert.DeserializeObject<IEnumerable<Prestamo>>(content);
+                var devoluciones = prestamos.Where(p => p.EstadoPrestamo != "Pendiente");
+                var pendientes = prestamos.Where(p => p.EstadoPrestamo == "Pendiente");
+                ViewBag.Devoluciones = devoluciones.Count();
+                ViewBag.CantidadPrestamos = prestamos.Count();
+                ViewBag.Pendientes = pendientes.Count();
+
+                var contentLibros = await responseLibros.Content.ReadAsStringAsync();
+                var libros = JsonConvert.DeserializeObject<IEnumerable<Libro>>(contentLibros);
+                ViewBag.Libros = libros.Count();                             
+                return View("Index", new { Prestamos = prestamos, Libros = libros });
+            }
+
             return View();
         }
 
